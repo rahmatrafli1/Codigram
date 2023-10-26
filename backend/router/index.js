cb25e4515dbfc28d8b228aef1fb4d77e1fc452a5
rahmatrafli1 <rahmatrafli1@gmail.com>
2023-10-26 11:15:53 +0700

    membuat middleware multer

--- backend/router/index.js
+++ backend/router/index.js
@@ -1,16 +1,5 @@
 const router = require("express").Router();
-const multer = require("multer");
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./assets/user");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
+const upload = require("../middleware/multer.js");
 
 const userRoutes = require("./user.js");
 router.use("/user", userRoutes);
