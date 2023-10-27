c71b1d8d70fd22696394fb08b3309d879a0e2626
rahmatrafli1 <rahmatrafli1@gmail.com>
2023-10-27 10:05:17 +0700

    membuat fitur list all post dan detail post backend

--- backend/controller/postController.js
+++ backend/controller/postController.js
@@ -1,6 +1,22 @@
+const { Post } = require("../models");
+
 class postController {
-  static async getAll(req, res) {}
-  static async detail(req, res) {}
+  static async getAll(req, res) {
+    const posts = await Post.findAll({ order: [["id", "ASC"]] });
+
+    res.status(200).json(posts);
+  }
+
+  static async detail(req, res) {
+    const detailPost = await Post.findOne({ where: { id: req.params.id } });
+
+    detailPost
+      ? res.status(200).json(detailPost)
+      : res
+          .status(404)
+          .json({ message: "Post id " + req.params.id + " tidak ada." });
+  }
+
   static async post(req, res) {}
   static async edit(req, res) {}
   static async delete(req, res) {}
