eefe5cd9c98a82cb739d086fcf3d2e61d60984ac
rahmatrafli1 <rahmatrafli1@gmail.com>
2023-10-29 19:29:49 +0700

    membuat tampilan login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./layout/Navbar";
+import { AiOutlineLogin } from "react-icons/ai";
 
 const Login = () => {
   // const [login, setLogin] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
       <Navbar />
-      <div className="container">
-        <h1>Login</h1>
+      <div className="container my-5">
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <div className="card">
+              <div className="card-title">
+                <h1 className="mt-2 text-center">Login</h1>
+              </div>
+              <div className="card-body">
+                <form>
+                  <div className="mb-3">
+                    <label className="form-label">Username</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Password</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="btn btn-primary d-flex align-items-center"
+                  >
+                    <AiOutlineLogin className="me-2" /> Login
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
